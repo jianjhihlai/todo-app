3e1ee39524a9d541abe58d44df1c263e251d9126
jjlai <jianjhihlai@gmail.com>
2022-02-05 14:13:06 +0800

    Vue todo function complete

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <TodoForm :about="about" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import TodoForm from "./components/TodoForm.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    TodoForm,
+  },
+  setup() {
+    const about = ref({
+      title: "Vue 3",
+      subTitle: "TypeScript demo",
+    });
+
+    return {
+      about,
+    };
+  },
+});
+</script>
+
+<style>
+@import "../node_modules/bootstrap/dist/css/bootstrap.css";
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoForm.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="mb-4">
+    <h1>{{ about.title }}</h1>
+    <h2>{{ about.subTitle }} {{ version }}</h2>
+  </div>
+  <form @submit.prevent="addNewTodo">
+    <div class="mb-5">
+      <label for="newTodo" class="form-label">New Todo</label>
+      <input
+        class="form-control"
+        id="newTodo"
+        placeholder="what's on your mind?"
+        v-model="newTodo"
+        name="newTodo"
+      />
+      <div class="m-2">
+        <button type="submit" class="btn btn-primary">Add New Todo</button>
+      </div>
+      <div class="m-2">
+        <button type="button" class="btn btn-danger" @click="removeAllTodos">Remove All</button>
+      </div>
+      <div class="m-2">
+        <button type="button" class="btn btn-success" @click="markAllDone">Mark All Done</button>
+      </div>
+    </div>
+    <div>
+      <div v-if="todos.length === 0">
+        <h3>Empty list 🥺</h3>
+      </div>
+      <div v-else>
+        <ul class="list-group">
+          <li
+            class="list-group-item d-flex flex-row justify-content-between align-items-center"
+            v-for="(todo, index) in todos"
+            :key="todo.id"
+          >
+            <h3 :class="{ mark: todo.done }" style="cursor: pointer" @click="toggleDone(todo)">
+              {{ todo.content }}
+            </h3>
+            <button type="button" class="btn btn-warning" @click="removeTodo(index)">
+              ✔ Done & Remove
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </form>
+</template>
+
+<script lang="ts">
+import { TodoType } from "@/models/todoModel";
+import { defineComponent, ref, PropType, onMounted } from "vue";
+import { v4 as uuidv4 } from "uuid";
+type Props = {
+  title: string;
+  subTitle: string;
+};
+
+export default defineComponent({
+  name: "TodoForm",
+  props: {
+    about: {
+      type: Object as PropType<Props>,
+      required: true,
+    },
+  },
+  setup(props) {
+    // local state
+    const version = ref("v1");
+    const newTodo = ref(""); // ref<string> is too verbose
+    // explicit type safe
+    const todos = ref<TodoType[]>([]);
+    function addNewTodo(): void {
+      if (!newTodo.value) return;
+      todos.value.push({
+        id: uuidv4(),
+        done: false,
+        content: newTodo.value,
+      });
+      console.log("newTodo:", newTodo.value);
+      newTodo.value = "";
+    }
+
+    function toggleDone(todo: TodoType): void {
+      todo.done = !todo.done;
+    }
+
+    function removeTodo(index: number): void {
+      todos.value.splice(index, 1);
+    }
+
+    function markAllDone(): void {
+      todos.value.forEach((todo) => (todo.done = true));
+    }
+
+    function removeAllTodos(): void {
+      todos.value = [];
+    }
+
+    onMounted(() => console.log(props.about.title));
+
+    return {
+      version,
+      todos,
+      newTodo,
+      addNewTodo,
+      toggleDone,
+      removeTodo,
+      markAllDone,
+      removeAllTodos,
+    }; // same as { version:version }
+  },
+});
+</script>
+
+<style>
+.mark {
+  text-decoration: line-through;
+}
+</style>
